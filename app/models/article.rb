c282b8d4d348075cfe976161ecb1f4ffe30112fc
pljacsk2 <paul.jackson624@gmail.com>
2016-07-11 17:27:04 -0700

    configured sql,added new users,comment section

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ActiveRecord::Base
 	belongs_to :user
-	validates :title, presence :true,
-		length: {maximum :40}
+	has_many :comments
+	validates :title, presence: true,
+		length: {maximum:40}
 	validates :post, presence: true
 end
